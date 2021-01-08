c6cd77e68904db6b7ac32321ee761b1db2bdb36e
rachel5004 <75432228+rachel5004@users.noreply.github.com>
2021-01-08 17:14:22 +0900

    capsule

--- 2021-1-7/src/달력만들기.java
+++ 2021-1-7/src/달력만들기.java
@@ -1,45 +1,25 @@
 import java.util.*;
 public class 달력만들기 {
+	
+	static void weeks() {
+		String[] strweek = {"일","월","화","수","목","금","토"};
+		for(String week:strweek)
+			System.out.print(week+"\t");
+		System.out.println();
+	}
 	static boolean LeanYear(int year) {
 		if((year%4==0 && year%100!=0)||(year%400==0))
 			return true;
 		else return false;
 	}
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("년도 입력:");
-		int year =  sc.nextInt();
-		System.out.print("월 입력:");
-		int month =  sc.nextInt();
-		System.out.println(year+"년 "+month+"월");
-		System.out.println("\n");
-		
-		String[] strweek = {"일","월","화","수","목","금","토"};
-		for(String week:strweek)
-			System.out.print(week+"\t");
-		System.out.println();
-		
-//		int week = 1;
-//		for(int i=1;i<=31;i++) {
-//			System.out.print(i+"\t");
-//			if(i%7==0) {
-//				System.out.println();
-//				week++;
-//		}
-		
-		int total = (year-1)*365+(year-1)/4
-								-(year-1)/100
-								+(year-1)/400;
-		int[] lastDay = {31,28,31,30,31,30,31,31,30,31,30,31};
-		if(LeanYear(year))
-			lastDay[1]=29;
-		else lastDay[1]=28;
+	static int getTotal(int month,int total, int[] lastDay) {
 		for(int i=0;i<month-1;i++) {
 			total+=lastDay[i];
 		}
 		total++;
-		int week = total%7;
+		return total;
+	}
+	static void getWeek(int[] lastDay, int month, int week) {
 		for(int i=1;i<=lastDay[month-1];i++) {
 			if(i==1) {
 				for(int j=0;j<week;j++) {
@@ -53,6 +33,44 @@ public class 달력만들기 {
 				System.out.println();
 			}
 		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("년도 입력:");
+		int year =  sc.nextInt();
+		System.out.print("월 입력:");
+		int month =  sc.nextInt();
+		System.out.println(year+"년 "+month+"월");
+		System.out.println("\n");
+		
+		weeks();
+		
+//		int week = 1;
+//		for(int i=1;i<=31;i++) {
+//			System.out.print(i+"\t");
+//			if(i%7==0) {
+//				System.out.println();
+//				week++;
+//		}
+		
+		int total = (year-1)*365+(year-1)/4
+								-(year-1)/100
+								+(year-1)/400;
+		int[] lastDay = {31,28,31,30,31,30,31,31,30,31,30,31};
+		
+		if(LeanYear(year))
+			lastDay[1]=29;
+		else lastDay[1]=28;
+//		
+//		for(int i=0;i<month-1;i++) {
+//			total+=lastDay[i];
+//		}
+//		total++;
+		total = getTotal(month, total, lastDay);
+		int week = total%7;
+		
+		getWeek(lastDay,month,week);
 	}
 
 }
--- 2021-1-7/src/숫자야구게임.java
+++ 2021-1-7/src/숫자야구게임.java
@@ -1,5 +1,47 @@
 import java.util.*;
 public class 숫자야구게임 {
+	static int[] userInput(int[] arr, int input) {
+		
+			arr[0]	= input/100;
+			arr[1] = (input%100)/10;
+			arr[2] = input%10;
+			return arr;
+	}
+	static boolean duple(int[] user) {
+		if (user[0]==user[1]||user[1]==user[2]||user[0]==user[2])
+			return true;
+		else return false;
+	}
+	static int[] score(int[] arr1, int[] arr2) {
+		int s=0;int b=0;
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				if(arr1[i]==arr2[j]) {
+					if(i==j) s++;
+					else b++;
+				}
+			}
+		}
+		int res[] = {s,b};
+		return res;
+		
+	}
+	static void result(int[] arr) {
+		System.out.println("=========결과========");
+		System.out.print("S: ");
+		for(int i=0;i<arr[0];i++) {
+			System.out.print("●");
+		}
+		System.out.println();
+		System.out.print("B: ");
+		for(int i=0;i<arr[1];i++) {
+			System.out.print("○");
+		}
+		System.out.println();
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int[] com = {3,6,9};
@@ -12,39 +54,24 @@ public class 숫자야구게임 {
 				System.out.println("잘못된 입력입니다");
 				continue;
 			}
-			user[0]	= input/100;
-			user[1] = (input%100)/10;
-			user[2] = input%10;
+			userInput(user,input);
 			
-			if(user[0]==user[1]||user[1]==user[2]||user[0]==user[2]) {
+			if(duple(user)) {
 				System.out.println("중복된 수는 사용할 수 없습니다");
 				continue;
 			}
 			
-			int s=0;int b=0;
-			for(int i=0;i<3;i++) {
-				for(int j=0;j<3;j++) {
-					if(com[i]==user[j]) {
-						if(i==j) s++;
-						else b++;
-					}
-				}
-			}
-			//System.out.printf("Input Number:%d, Result:%dS-%dB\n",input,s,b);
+			int[] score = score(com,user);
+			
+//			System.out.printf("Input Number:%d, Result:%dS-%dB\n",input,score[0],score[1]);
+//			if(score[0]==3) {
+//				System.out.println("Game clear");
+//				break;
+//			}
 			
 			/// 전광판
-			System.out.println("=========결과========");
-			System.out.print("S: ");
-			for(int i=0;i<s;i++) {
-				System.out.print("●");
-			}
-			System.out.println();
-			System.out.print("B: ");
-			for(int i=0;i<b;i++) {
-				System.out.print("○");
-			}
-			System.out.println();
-			if(s==3) {
+			result(score);
+			if(score[0]==3) {
 				System.out.println("Game clear");
 				break;
 			}
