a53fb90f4d2bdd2eac2710a6db399cfddf509cec
rachel5004 <75432228+rachel5004@users.noreply.github.com>
2021-02-05 15:37:35 +0900

    network project

---
+++ 20210203-네트워크활용2/src/com/sist/network/Client.java
@@ -0,0 +1,16 @@
+package com.sist.network;
+import java.net.*;
+import java.io.*;
+public class Client {
+
+	public static void main(String[] args) {
+        try {
+        	Socket s=new Socket("localhost",3355);
+        	BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));
+        	// s=서버
+        	String msg=in.readLine();
+        	System.out.println(msg);
+        }catch(Exception ex) {}
+	}
+
+}
